759ced90c480fa95163d251079ef16b5a244adb1
adamjonas <jonas@flatironschool.com>
2018-07-01 19:16:46 -0400

    Initial Commit

---
+++ tx.py
@@ -0,0 +1,64 @@
+from io import BytesIO
+from unittest import TestCase
+
+from helper import (
+    little_endian_to_int,
+    read_varint,
+)
+from script import Script
+
+
+class Tx:
+
+    def __init__(self, version, tx_ins, tx_outs, locktime):
+        self.version = version
+        self.tx_ins = tx_ins
+        self.tx_outs = tx_outs
+        self.locktime = locktime
+
+    def __repr__(self):
+        tx_ins = ''
+        for tx_in in self.tx_ins:
+            tx_ins += tx_in.__repr__() + '\n'
+        tx_outs = ''
+        for tx_out in self.tx_outs:
+            tx_outs += tx_out.__repr__() + '\n'
+        return 'version: {}\ntx_ins:\n{}\ntx_outs:\n{}\nlocktime: {}\n'.format(
+            self.version,
+            tx_ins,
+            tx_outs,
+            self.locktime,
+        )
+
+
+class TxIn:
+    def __init__(self, prev_tx, prev_index, script_sig, sequence):
+        self.prev_tx = prev_tx
+        self.prev_index = prev_index
+        self.script_sig = Script.parse(script_sig)
+        self.sequence = sequence
+
+    def __repr__(self):
+        return '{}:{}'.format(
+            self.prev_tx.hex(),
+            self.prev_index,
+        )
+
+
+class TxOut:
+
+    def __init__(self, amount, script_pubkey):
+        self.amount = amount
+        self.script_pubkey = Script.parse(script_pubkey)
+
+    def __repr__(self):
+        return '{}:{}'.format(self.amount, self.script_pubkey)
+
+
+class TxTest(TestCase):
+
+    def test_parse_locktime(self):
+        raw_tx = bytes.fromhex('0100000001813f79011acb80925dfe69b3def355fe914bd1d96a3f5f71bf8303c6a989c7d1000000006b483045022100ed81ff192e75a3fd2304004dcadb746fa5e24c5031ccfcf21320b0277457c98f02207a986d955c6e0cb35d446a89d3f56100f4d7f67801c31967743a9c8e10615bed01210349fc4e631e3624a545de3f89f5d8684c7b8138bd94bdd531d2e213bf016b278afeffffff02a135ef01000000001976a914bc3b654dca7e56b04dca18f2566cdaf02e8d9ada88ac99c39800000000001976a9141c4bc762dd5423e332166702cb75f40df79fea1288ac19430600')
+        stream = BytesIO(raw_tx)
+        tx = Tx.parse(stream)
+        self.assertEqual(tx.locktime, 410393)
